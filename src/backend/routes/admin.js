e5320e5dccc8c253802b70760c05a28025e5e641
Asaad Abbasi <asaad.abbasi@gmail.com>
2022-11-23 23:05:22 +0500

    changes in registration & login
    - Proper error messages when signing up or registering
    
    - When registering check across all the user types for the same email address
    
    - Implemented storing hashed passwords

--- src/backend/routes/admin.js
+++ src/backend/routes/admin.js
@@ -63,7 +63,6 @@ router.get("/", (req, res) => {
               error: "No Pending Approvals",
             });
           } else {
-            //
             let resultJSON = JSON.parse(JSON.stringify(result));
             resultJSON.forEach((r) => {
               r.images = r.images.split(",");
--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const randtoken = require("rand-token");
+const bcrypt = require("bcrypt");
 
 const db = require("../configs/database");
 const returnLoginPayloadResponse =
@@ -41,43 +42,48 @@ router.post("/login/admin", (req, res) => {
 
 ///////////////////////////////
 
+//Freelancer, Client, Comapny Client Login
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
   db.query(
-    `select user_type , freelancer_id as id from freelancer where email="${email}" and password="${password}" UNION 
-    select user_type , company_client_id as id from company_client where email="${email}" and password="${password}" UNION
-    select user_type , client_id as id from client where email="${email}" and password="${password}"`,
-    (e, r, f) => {
+    `select user_type, password, freelancer_id as id from freelancer where email="${email}" UNION 
+    select user_type, password, company_client_id as id from company_client where email="${email}" UNION
+    select user_type, password, client_id as id from client where email="${email}";`,
+    (e, r) => {
       if (e) {
-        res.status(500).send({
-          error: e.message,
-        });
+        res.status(500).send({ error: e.message });
       } else if (r.length > 0) {
-        if (r[0].user_type === "freelancer") {
-          returnLoginPayloadResponse(
-            res,
-            `SELECT * from freelancer where freelancer_id="${r[0].id}"`
-          );
-        } else if (r[0].user_type === "company_client") {
-          returnLoginPayloadResponse(
-            res,
-            `SELECT * from company_client where company_client_id="${r[0].id}"`
-          );
-        } else if (r[0].user_type === "client") {
-          returnLoginPayloadResponse(
-            res,
-            `SELECT * from client where client_id="${r[0].id}"`
-          );
-        }
-      } else {
-        res.status(400).send({
-          error: "Invalid email or password",
+        //Since email found compare Hashed Password
+        bcrypt.compare(password, r[0]["password"], (err, result) => {
+          if (result) {
+            if (r[0].user_type === "freelancer") {
+              returnLoginPayloadResponse(
+                res,
+                `SELECT * from freelancer where freelancer_id="${r[0].id}"`
+              );
+            } else if (r[0].user_type === "company_client") {
+              returnLoginPayloadResponse(
+                res,
+                `SELECT * from company_client where company_client_id="${r[0].id}"`
+              );
+            } else if (r[0].user_type === "client") {
+              returnLoginPayloadResponse(
+                res,
+                `SELECT * from client where client_id="${r[0].id}"`
+              );
+            }
+          } else {
+            res.status(400).send({ error: "Invalid Password" });
+          }
         });
+      } else {
+        res.status(400).send({ error: "Email Address Not Found" });
       }
     }
   );
 });
 
+//Email Verification
 router.get("/verify/:uid", (req, res) => {
   const { uid } = req.params;
   const result = emailVerificationSchema.find({ uid: uid }, (err, data) => {
@@ -134,115 +140,160 @@ router.get("/verify/:uid", (req, res) => {
   });
 });
 
+//Registering All Users Types
 router.post("/register/:userType", (req, res) => {
   const { userType } = req.params;
-  let generatedID = randtoken.uid(45);
-  if (userType === "freelancer" || userType === "client") {
-    const {
-      first_name,
-      last_name,
-      username,
-      email,
-      password,
-      gender,
-      phone_number,
-      region,
-      country,
-      state,
-    } = req.body;
+  const { email, phone_number, username, password } = req.body;
 
-    let query = "";
-    if (userType === "freelancer") {
-      query = `INSERT INTO freelancer(freelancer_id,first_name,last_name,username,email,password,gender,phone_number,region,country,state) VALUES ("${generatedID}","${first_name}","${last_name}","${username}","${email}","${password}","${gender}","${phone_number}","${region}","${country}","${state}");`;
-    } else {
-      query = `INSERT INTO client(client_id,first_name,last_name,username,email,password,gender,phone_number,region,country,state) VALUES ("${generatedID}","${first_name}","${last_name}","${username}","${email}","${password}","${gender}","${phone_number}","${region}","${country}","${state}");`;
+  //Check if email already registered across multiple user types;
+  db.query(
+    `select freelancer_id as id from freelancer where email="${email}" UNION 
+    select company_client_id as id from company_client where email="${email}" UNION
+    select client_id as id from client where email="${email}";`,
+    (e, r) => {
+      if (e) {
+        res.status(500).send({ error: e.message });
+      } else if (r.length <= 0) {
+        //Check if phone number already registered across multiple user types;
+        db.query(
+          `select user_type , freelancer_id as id from freelancer where phone_number="${phone_number}" UNION 
+          select user_type , company_client_id as id from company_client where phone_number="${phone_number}" UNION
+          select user_type , client_id as id from client where phone_number="${phone_number}";`,
+          (e, r) => {
+            if (e) {
+              res.status(500).send({ error: e.message });
+            } else if (r.length <= 0) {
+              //Check if username already registered across multiple client & freelancer types;
+              db.query(
+                `select user_type , freelancer_id as id from freelancer where username="${username}" UNION 
+                select user_type , client_id as id from client where username="${username}"; `,
+                (e, r) => {
+                  if (e) {
+                    res.status(500).send({ error: e.message });
+                  } else if (r.length <= 0) {
+                    let generatedID = randtoken.uid(45); //Generate UserID
+                    bcrypt.hash(password, 10, function (err, hash) {
+                      //Generate Hash Password
+                      if (userType === "freelancer" || userType === "client") {
+                        const {
+                          first_name,
+                          last_name,
+                          username,
+                          gender,
+                          region,
+                          country,
+                          state,
+                        } = req.body;
+                        let query = "";
+                        if (userType === "freelancer") {
+                          query = `INSERT INTO freelancer(freelancer_id,first_name,last_name,username,email,password,gender,phone_number,region,country,state) VALUES ("${generatedID}","${first_name}","${last_name}","${username}","${email}","${hash}","${gender}","${phone_number}","${region}","${country}","${state}");`;
+                        } else {
+                          query = `INSERT INTO client(client_id,first_name,last_name,username,email,password,gender,phone_number,region,country,state) VALUES ("${generatedID}","${first_name}","${last_name}","${username}","${email}","${hash}","${gender}","${phone_number}","${region}","${country}","${state}");`;
+                        }
+                        db.query(query, (e, r, f) => {
+                          if (e) {
+                            if (e["sqlMessage"].includes("username_UNIQUE")) {
+                              res
+                                .status(500)
+                                .send({ error: "Username already taken" });
+                            } else {
+                              res.status(500).send({
+                                error: e.message,
+                              });
+                            }
+                          } else if (r) {
+                            db.query(
+                              `INSERT into profile (profile_id,user_type) values ("${generatedID}" , "${userType}");`,
+                              (e, r, f) => {
+                                if (e) {
+                                  res.status(500).send({
+                                    error: e.message,
+                                  });
+                                } else if (r) {
+                                  generateEmailVerification(res, {
+                                    email,
+                                    userType,
+                                  });
+                                } else {
+                                  res.status(400).send({
+                                    error: "Error Creating Profile",
+                                  });
+                                }
+                              }
+                            );
+                          } else {
+                            res.status(400).send({
+                              error: "Error registering freelancer",
+                            });
+                          }
+                        });
+                      } else if (userType === "company-client") {
+                        const { company_name, country, region } = req.body;
+                        let query = `INSERT INTO company_client(company_client_id,email,password,company_name,phone_number,country,region) VALUES ("${generatedID}","${email}","${hash}","${company_name}","${phone_number}","${country}","${region}");`;
+                        db.query(query, (e, r, f) => {
+                          if (e) {
+                            if (
+                              e["sqlMessage"].includes("company_name_UNIQUE")
+                            ) {
+                              res.status(500).send({
+                                error: "Company Name already registered",
+                              });
+                            } else {
+                              res.status(500).send({
+                                error: e.message,
+                              });
+                            }
+                          } else if (r) {
+                            db.query(
+                              `INSERT into profile (profile_id,user_type) values ("${generatedID}" , "company_client");`,
+                              (e, r, f) => {
+                                if (e) {
+                                  res.status(500).send({
+                                    error: e.message,
+                                  });
+                                } else if (r) {
+                                  generateEmailVerification(res, {
+                                    email,
+                                    userType: "company_client",
+                                  }); //Generating email verification link
+                                } else {
+                                  res.status(400).send({
+                                    error: "Error Creating Profile",
+                                  });
+                                }
+                              }
+                            );
+                          } else {
+                            res.status(400).send({
+                              error: "Error registering company",
+                            });
+                          }
+                        });
+                      } else {
+                        res.status(400).send("Invalid api endpoint");
+                      }
+                    });
+                  } else {
+                    res.status(400).send({
+                      error: "Username Already Registered",
+                    });
+                  }
+                }
+              );
+            } else {
+              res.status(400).send({
+                error: "Phone number already registered",
+              });
+            }
+          }
+        );
+      } else {
+        res.status(400).send({
+          error: "Email Already Registered",
+        });
+      }
     }
-
-    db.query(query, (e, r, f) => {
-      if (e) {
-        res.status(500).send({
-          error: e.message,
-        });
-      } else if (r) {
-        db.query(
-          `INSERT into profile (profile_id,user_type) values ("${generatedID}" , "${userType}");`,
-          (e, r, f) => {
-            if (e) {
-              res.status(500).send({
-                error: e.message,
-              });
-            } else if (r) {
-              generateEmailVerification(res, { email, userType });
-            } else {
-              res.status(400).send({
-                error: "Error Creating Profile",
-              });
-            }
-          }
-        );
-      } else {
-        res.status(400).send({
-          error: "Error registering freelancer",
-        });
-      }
-    });
-  } else if (userType === "company-client") {
-    const { email, password, company_name, phone_number, country, region } =
-      req.body;
-    let query = `INSERT INTO company_client(company_client_id,email,password,company_name,phone_number,country,region) VALUES ("${generatedID}","${email}","${password}","${company_name}","${phone_number}","${country}","${region}");`;
-    db.query(query, (e, r, f) => {
-      if (e) {
-        res.status(500).send({
-          error: e.message,
-        });
-      } else if (r) {
-        db.query(
-          `INSERT into profile (profile_id,user_type) values ("${generatedID}" , "company_client");`,
-          (e, r, f) => {
-            if (e) {
-              res.status(500).send({
-                error: e.message,
-              });
-            } else if (r) {
-              generateEmailVerification(res, {
-                email,
-                userType: "company_client",
-              }); //Generating email verification link
-            } else {
-              res.status(400).send({
-                error: "Error Creating Profile",
-              });
-            }
-          }
-        );
-      } else {
-        res.status(400).send({
-          error: "Error registering company",
-        });
-      }
-    });
-  } else if (userType === "admin") {
-    const { email, password, username } = req.body;
-    let query = `INSERT INTO administrator(admin_id,email,password,username) VALUES ("${generatedID}","${email}","${password}","${username}");`;
-    db.query(query, (e, r, f) => {
-      if (e) {
-        res.status(400).send({
-          error:
-            "Administrator already registered with the same username or email",
-        });
-      } else if (r) {
-        res.status(200).send({
-          message: "Registration Successsful",
-        });
-      } else {
-        res.status(400).send({
-          error: "Error registering company",
-        });
-      }
-    });
-  } else {
-    res.status(400).send("Invalid api endpoint");
-  }
+  );
 });
 
 module.exports = router;
